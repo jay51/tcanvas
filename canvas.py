ba0ce274bd01628e598697c61595a33d1811b4c9
jay51 <nenojafaar@gmail.com>
2023-10-31 00:51:22 -0500

    refactor

--- canvas.py
+++ canvas.py
@@ -3,11 +3,15 @@
 
 class Canvas():
 
-    def __init__(self, rows, cols):
+    def __init__(self, rows, cols, canvas=None, vert_bar='|', horz_bar='-'):
         self.CANVAS_ROWS = rows # vertical
         self.CANVAS_COLS = cols # horozontal
         self.CANVAS_SIZE = self.CANVAS_ROWS * self.CANVAS_COLS
-        self.canvas = self.make_canvas()
+        # to provid a pre-set canvas
+        self.canvas = self.make_canvas() if canvas is None else canvas
+
+        self.vert_bar = vert_bar
+        self.horz_bar = horz_bar
 
         self.UP      = 'U'
         self.DOWN    = 'D'
@@ -26,49 +30,63 @@ class Canvas():
 
             row = ' '.join([ self.canvas[c] for c in range(row_start, row_start + self.CANVAS_COLS) ])
             print(row)
-            
 
 
 
-    def draw_line(self, starting_pos: int, steps: int, dirct: str) -> None:
+
+    def set_row_col(self, row, col, steps, dirct):
+        pos = (row * self.CANVAS_COLS) + col
+        self.set_at_idx(pos, steps, dirct)
+
+
+    def set_at_idx(self, starting_pos: int, steps: int, dirct: str) -> None:
         if dirct == self.LEFT:
             for i in range(steps):
                 # stop from drawing on next row
                 row_n = starting_pos // self.CANVAS_ROWS
-                left_most = (row_n * self.CANVAS_ROWS)
+                left_most = (row_n * self.CANVAS_COLS)
 
-                if starting_pos - i < left_most:
-                    break
+                #self.print_info()
+                #print(f'row_n: {row_n}')
+                #print(f'left_most: {left_most}')
+                #print(f'starting_pos: {starting_pos}')
 
-                self.canvas[starting_pos - i] = '-'
+                #if starting_pos - i < left_most:
+                    #break
+
+                self.canvas[starting_pos - i] = self.horz_bar
 
 
         elif dirct == self.RIGHT:
             for i in range(steps):
                 # stop from drawing on next row
                 row_n = starting_pos // self.CANVAS_ROWS
-                rigth_most = (row_n * self.CANVAS_ROWS) + self.CANVAS_COLS
+                rigth_most = (row_n * self.CANVAS_COLS) + self.CANVAS_COLS
 
-                if starting_pos + i > rigth_most -1:
-                    break
+                #if starting_pos + i > rigth_most -1:
+                    #break
                 
-                self.canvas[starting_pos + i] = '-'
+                self.canvas[starting_pos + i] = self.horz_bar
 
         elif dirct == self.UP:
             for i in range(steps):
                 idx = starting_pos - (i * self.CANVAS_COLS) if  starting_pos - (i * self.CANVAS_COLS) > 0 else starting_pos
+                # print(f'idx: {idx}')
 
-                print(f'idx: {idx}')
-
-                self.canvas[idx] = '|'
+                self.canvas[idx] = self.vert_bar
 
 
         elif dirct == self.DOWN:
             for i in range(steps):
                 # idx = (starting_pos + (i * 20)) % CANVAS_SIZE make it wrap
                 idx = starting_pos + (i * self.CANVAS_COLS) if  starting_pos + (i * self.CANVAS_COLS) < self.CANVAS_SIZE else starting_pos
-                self.canvas[idx] = '|'
+                self.canvas[idx] = self.vert_bar
 
 
+
+    def print_info(self):
+        print(f'{self.CANVAS_SIZE=}')
+        print(f'{self.CANVAS_COLS=}')
+        print(f'{self.CANVAS_ROWS=}')
 
 
--- main.py
+++ main.py
@@ -3,17 +3,32 @@ import canvas
 
 
 def main():
-    c = canvas.Canvas(30,20)
+    c = canvas.Canvas(10,20, None, 'A', 'B')
 
-    starting_pos = ((c.CANVAS_ROWS * c.CANVAS_COLS) // 2) + 10
+    # canv_mid = (((c.CANVAS_ROWS - 3) * c.CANVAS_COLS) // 2) + 10
+    # print(canv_mid)
 
-    # c.draw_line(5, 3, c.LEFT)
+    # canv_mid = (c.CANVAS_COLS * 3) + (c.CANVAS_COLS // 2)
+    # c.set_at_idx(canv_mid, 4, c.LEFT)
+
+    #print((c.CANVAS_COLS // 5) * 3)
+
+    #c.set_row_col(3, 0, 4, c.LEFT)
+    # c.set_row_col(3, 10,  7, c.LEFT)
+
+    canv_mid = (c.CANVAS_COLS * 3) + (c.CANVAS_COLS // 2)
+    c.set_at_idx(15, 4, c.LEFT)
+
+    # c.draw_line(5, 4, c.DOWN)
+    # c.draw_line(5, 4, c.RIGHT)
+    # c.draw_line(5, 4, c.UP)
+
     # c.draw_line(19, 3, c.RIGHT)
 
     # c.draw_line(5, 3, c.LEFT)
     # c.draw_line(10, 10, c.DOWN)
 
-    c.draw_line(300, 5, c.UP)
+    # c.draw_line(300, 5, c.UP)
 
     c.draw_canvas()
 
