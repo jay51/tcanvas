a615f3efa7d6b4ee9f7d99089c8c5f8a0ca387f8
jay51 <nenojaafar3@gmail.com>
2024-05-13 18:09:38 -0500

    add turtle game

--- main.py
+++ main.py
@@ -1,22 +1,70 @@
 from tcanvas import canvas
 from test import draw_face, draw_face2
 
+from termios import tcgetattr, TCSADRAIN, tcsetattr
+import math
+import os
+import sys, tty, select
+
+def read(f=sys.stdin, old_attr=tcgetattr(sys.stdin.fileno()), time=1, chars=1):
+    '''
+        cooperative io read from stdin/terminal without pressing enter
+    '''
+
+    try:
+        tty.setraw(f.fileno())
+        rlist, o, e = select.select([f], [], [], time)
+    finally:
+        tcsetattr(f.fileno(), TCSADRAIN, old_attr)
+
+    if sys.stdin in rlist:
+        return sys.stdin.read(chars)
+
+    return ''
 
 
 def main():
-    c = canvas.Canvas(30,50, None, 'A', 'B')
-    draw_face2(c)
+    t = canvas.Turutle(0,0)
 
-    while False:
-        move = input('move ex. 1f: ')
-        steps = move[0]
-        dirct = move[1]
+    f = sys.stdin
+    old_attr = tcgetattr(f.fileno())
 
-        if dirct == c.UP:
-            pass
+    while True:
+
+        t.display()
+        t.forward()
+        # t.print_info()
+
+        buff = read(f, old_attr)
+        if buff == 'f':
+            t.forward()
+        if buff == 'r':
+            t.right()
+        if buff == 'l':
+            t.left()
+        if buff == 'e':
+            exit()
 
 
 
 
-if __name__ == '__main__':
+
+
+
+if os.getenv('TEST'):
+    pass
+    # Example find angel
+    #angle = angle_between_points(x1, y1, x2, y2)
+    #print("Angle between points:", angle)
+
+    # Example Rotation angle in degrees
+    #rotated_x, rotated_y = rotate_matrix(x2, y2, 180)
+    # vector = [x2, y2]
+    # angle = -45
+    # rotated_vector = rotate_vector(vector, angle)
+
+    # print("Original vector:", vector)
+    # print("Rotated vector:", rotated_vector)
+
+elif __name__ == '__main__':
     main()
