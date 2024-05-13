b95cb7e328de1b6d52986e82ca5dd3ddde0bee98
jay51 <nenojaafar3@gmail.com>
2024-05-13 17:49:06 -0500

    improve canvas abalities

--- tcanvas/canvas.py
+++ tcanvas/canvas.py
@@ -1,17 +1,17 @@
 
+import math
 
 
 class Canvas():
 
-    def __init__(self, rows, cols, canvas=None, vert_bar='|', horz_bar='-'):
+    def __init__(self, rows, cols, canvas=None, symbol='>'):
         self.CANVAS_ROWS = rows # vertical
         self.CANVAS_COLS = cols # horozontal
         self.CANVAS_SIZE = self.CANVAS_ROWS * self.CANVAS_COLS
         # to provid a pre-set canvas
         self.canvas = self.make_canvas() if canvas is None else canvas
 
-        self.vert_bar = vert_bar
-        self.horz_bar = horz_bar
+        self.symbol = symbol
 
         self.UP      = 'U'
         self.DOWN    = 'D'
@@ -21,7 +21,12 @@ class Canvas():
     def make_canvas(self) -> [str]:
         return ['.' for c in range(self.CANVAS_ROWS * self.CANVAS_COLS)]
 
+    def set_symbol(self, symbol):
+        self.symbol = symbol
 
+
+    def cls(self):
+        print('\x1b[2J\x1b[H', end='')
 
     def draw_canvas(self):
         for i in range(self.CANVAS_ROWS):
@@ -32,27 +37,26 @@ class Canvas():
             print(row)
 
 
+    def set_xy(self, x, y, steps, dirct):
+        # this method is deprecated
+        self.set_row_col(y, x , steps, dirct)
+        # self.set_cell(x, y)
+
     def set_row_col(self, row, col, steps, dirct):
+        #       Row(find row) + col
         pos = (row * self.CANVAS_COLS) + col
         self.set_at_idx(pos, steps, dirct)
 
 
     def set_at_idx(self, starting_pos: int, steps: int, dirct: str) -> None:
+        # this function is deprecated
         if dirct == self.LEFT:
             for i in range(steps):
                 # stop from drawing on next row
                 row_n = starting_pos // self.CANVAS_ROWS
                 left_most = (row_n * self.CANVAS_COLS)
 
-                #self.print_info()
-                #print(f'row_n: {row_n}')
-                #print(f'left_most: {left_most}')
-                #print(f'starting_pos: {starting_pos}')
-
-                #if starting_pos - i < left_most:
-                    #break
-
-                self.canvas[starting_pos - i] = self.horz_bar
+                self.canvas[starting_pos - i] = self.symbol
 
 
         elif dirct == self.RIGHT:
@@ -61,24 +65,26 @@ class Canvas():
                 row_n = starting_pos // self.CANVAS_ROWS
                 rigth_most = (row_n * self.CANVAS_COLS) + self.CANVAS_COLS
 
-                #if starting_pos + i > rigth_most -1:
-                    #break
-                
-                self.canvas[starting_pos + i] = self.horz_bar
+                self.canvas[starting_pos + i] = self.symbol
 
         elif dirct == self.UP:
             for i in range(steps):
                 idx = starting_pos - (i * self.CANVAS_COLS) if  starting_pos - (i * self.CANVAS_COLS) > 0 else starting_pos
-                # print(f'idx: {idx}')
 
-                self.canvas[idx] = self.vert_bar
+                self.canvas[idx] = self.symbol
 
 
         elif dirct == self.DOWN:
             for i in range(steps):
-                # idx = (starting_pos + (i * 20)) % CANVAS_SIZE make it wrap
                 idx = starting_pos + (i * self.CANVAS_COLS) if  starting_pos + (i * self.CANVAS_COLS) < self.CANVAS_SIZE else starting_pos
-                self.canvas[idx] = self.vert_bar
+                self.canvas[idx] = self.symbol
+
+    def set_cell(self, x, y, symbol=None):
+        #       Row(find row) + col
+        pos = (y * self.CANVAS_COLS) + x
+        row_n = pos // self.CANVAS_ROWS
+        rigth_most = (row_n * self.CANVAS_COLS) + self.CANVAS_COLS
+        self.canvas[pos] = self.symbol
 
 
 
